2ae2bd8c766f447d4a2c691fd14a8c595de33a89
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2022-12-12 16:48:02 +0800

    pizza

--- Lesson2-Pizza.dart
+++ Lesson2-Pizza.dart
@@ -1,34 +1,41 @@
 import 'dart:io';
 
 void main() {
-  print('==============================================================');
-  print('Pizza Price "Small: RM5.00, Medium: RM 7.00, Large: RM 10.00"');
-  print('Please enter your pizza size (small, medium, or large): ');
-  String pizza_size = stdin.readLineSync()!;
+  String? loop;
+  do {
+    print('==============================================================');
+    print('Pizza Price "Small: RM5.00, Medium: RM 7.00, Large: RM 10.00"');
+    print('Please enter your pizza size (small, medium, or large): ');
+    String pizza_size = stdin.readLineSync()!;
 
-  print('How many pizza do you want of size $pizza_size');
-  int qty = int.parse(stdin.readLineSync()!);
+    if (pizza_size == 'small' ||
+        pizza_size == 'medium' ||
+        pizza_size == 'large') {
+      print('How many pizza do you want of size $pizza_size');
+      int qty = int.parse(stdin.readLineSync()!);
 
-  var price;
-  var full_price;
+      var price;
+      var full_price;
 
-  switch (pizza_size) {
-    case 'small':
-      price = 5;
+      switch (pizza_size) {
+        case 'small':
+          price = 5;
+          break;
+        case 'medium':
+          price = 7;
+          break;
+        case 'large':
+          price = 10;
+          break;
+      }
+
       full_price = price * qty;
       print('Your Total Payment is :$full_price');
-      break;
-    case 'medium':
-      price = 7;
-      full_price = price * qty;
-      print('Your Total Payment is :$full_price');
-      break;
-    case 'large':
-      price = 10;
-      full_price = price * qty;
-      print('Your Total Payment is :$full_price');
-      break;
-    default:
+      print('Do you want to make a new order?(y,n)');
+      loop = stdin.readLineSync()!;
+    } else {
       print('Invalid Pizza Size Input. Please Run This Program Again!!');
-  }
+      loop = 'Y';
+    }
+  } while (loop == 'Y' || loop == 'y');
 }
