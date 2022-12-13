f8e99a3e057d96e0bbf44cc36ecf1fa09976031e
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2022-12-13 12:33:51 +0800

    Object and class full

--- Lesson3 - Class & Object.dart
+++ Lesson3 - Class & Object.dart
@@ -1,6 +1,8 @@
 void main() {
-  var pelajar = new Pelajar('Zainal', 12, 'Malay', 'Malaysia', 'Kuantan');
-  var pelajar2 = new Pelajar('Amirah', 12, 'Malay', 'Malaysia', 'Pekan');
+  var pelajar = new Pelajar('Zainal', ' Kuantan',
+      age: 12, race: 'Malay', country: 'Malaysia', kelas: '2 Zamrud');
+  var pelajar2 = new Pelajar('Amirah', 'Pekan',
+      country: 'Malaysia', race: 'Malay', age: 12, kelas: '2 Zamrud');
 
   pelajar.details();
   pelajar2.details();
@@ -11,13 +13,39 @@ class Pelajar {
   int age;
   String race;
   String country;
-  String city;
+  String? city;
+  String? kelas;
 
-  Pelajar(this.name, this.age, this.race, this.country, this.city);
+  Pelajar(this.name, this.city,
+      {required this.age,
+      required this.race,
+      required this.country,
+      required this.kelas});
 
   void details() {
     print(name);
     print(age);
     print(city);
   }
+}
+
+class Teacher extends Pelajar {
+  String carType;
+  Teacher(super.name, super.city,
+      {required super.age,
+      required super.race,
+      required super.country,
+      required super.kelas,
+      required this.carType});
+}
+
+class Pentadbir extends Teacher {
+  String? pejabat;
+  Pentadbir(super.name, super.city,
+      {required super.age,
+      required super.race,
+      required super.kelas,
+      required super.country,
+      required super.carType,
+      this.pejabat});
 }
